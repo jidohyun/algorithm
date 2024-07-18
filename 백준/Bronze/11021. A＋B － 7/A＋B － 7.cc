36a67b571074f94fbd2d4a3af6f878b4f3d69d3e
도현 <shickcoong777@gmail.com>
2024-07-18 15:41:27 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, c, sum;
+  cin >> c;
+  for (int i = 1; i <= c; i++)
+  {
+    cin >> a >> b;
+    printf("Case #%d: %d\n", i, a + b);
+  }
+  
+  
+}
