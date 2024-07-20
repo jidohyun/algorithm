423743e6568a2a6ac0822f7ef2c909cf9d531250
도현 <shickcoong777@gmail.com>
2024-07-20 16:12:19 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <numeric>
+#include <utility>
+using namespace std;
+
+int main() {
+  // ios::sync_with_stdio(false), cin.tie(nullptr),cout.tie(nullptr);
+  string a;
+  cin >> a;
+  cout << a.length();
+  
+}
