d9c25eb825596c5c72d21855cc879e653a5d415f
도현 <shickcoong777@gmail.com>
2024-07-26 23:30:00 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b = 0;
+    for(int i = 0; i < 5; i++) {
+        cin >> a;
+        b += a * a;
+    }
+    cout << b % 10;
+}
