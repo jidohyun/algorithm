8cefe58e1cb7408c861a3579371e284fc5700eef
도현 <shickcoong777@gmail.com>
2024-08-31 03:58:28 +0900

    [Bronze IV] Title: 주사위, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cin >> a;
+    for(int i = 1; i <= a; i++) {
+        cin >> b >> c;
+        printf("Case %d: %d\n",i, b+c);
+    }
+}
