2f01dfdc6143cf216b517835d0d149cf05add3f6
도현 <shickcoong777@gmail.com>
2024-09-04 21:45:16 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+  int a, b, c, d, e, f;
+  cin >> a >> b >> c >> d >> e >> f;
+  printf("%d %d %d %d %d %d", 1 - a, 1 - b, 2 - c, 2 - d, 2 - e, 8 - f);
+}
